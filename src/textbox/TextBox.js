239d28e0b69cddab3eeddfa67e4f53b107a03966
Kenneth Lee <klee33@uw.edu>
2023-11-15 23:08:27 -0500

    Add textbox and font

--- src/textbox/TextBox.js
+++ src/textbox/TextBox.js
@@ -3,6 +3,13 @@ import './TextBox.css';
 function TextBox() {
   return (
     <div className="TextBoxContainer">
+      <span className="TextBoxTitle">Summarize your text below</span>
+      <form>
+        <div className="FormContainer">
+          <textarea className="TextBox" spellcheck="false"></textarea>
+          <button className="TextBoxButton">Summarize Text</button>
+        </div>
+      </form>
     </div>
   );
 }
