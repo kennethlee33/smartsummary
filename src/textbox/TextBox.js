58526feafd2ec71b853a2fba4eae36eaaf7cee35
Kenneth Lee <klee33@uw.edu>
2023-11-18 22:15:15 -0500

    Disable summarization button when textbox empty

--- src/textbox/TextBox.js
+++ src/textbox/TextBox.js
@@ -1,17 +1,47 @@
 import './TextBox.css';
+import React from 'react';
 
-function TextBox() {
-  return (
-    <div className="TextBoxContainer">
-      <span className="TextBoxTitle">Summarize your text below 👇</span>
-      <form>
-        <div className="FormContainer">
-          <textarea className="TextBox" spellcheck="false"></textarea>
-          <button className="TextBoxButton">Summarize Text</button>
-        </div>
-      </form>
-    </div>
-  );
+class TextBox extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: ""
+    };
+  }
+
+  handleTextChange = (event) => {
+    this.setState({text: event.target.value});
+  }
+
+  isTextBoxEmpty = () => {
+    if (this.state.text === "") {
+      return true;
+    }
+
+    return false;
+  }
+
+  render() {
+    return (
+      <div className="TextBoxContainer">
+        <span className="TextBoxTitle">Summarize your text below 👇</span>
+        <form>
+          <div className="FormContainer">
+            <textarea
+              className="TextBox"
+              value={this.state.text}
+              onChange={this.handleTextChange}
+              spellCheck="false" 
+            />
+            <button
+              className="TextBoxButton"
+              disabled={this.isTextBoxEmpty()}>Summarize Text</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default TextBox;
