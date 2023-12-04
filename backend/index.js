e4e3e75ae4d7476a5135d301a56ef6560f36f923
Kenneth Lee <klee33@uw.edu>
2023-12-03 19:04:49 -0500

    Add NodeJS backend functionality

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
+
+app.use(bodyParser.text());
+
+app.post("/summarize", (req, res) => {
+    console.log(req.body);
+    res.json({ message: 'POST request response'})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
--- src/textbox/TextBox.js
+++ src/textbox/TextBox.js
@@ -18,15 +18,31 @@ class TextBox extends React.Component {
     if (this.state.text.trim() === "") {
       return true;
     }
-
     return false;
+  }
+
+  sendTextForSummarization = () => {
+    fetch("http://localhost:3001/summarize", {
+      method: 'POST',
+      body: this.state.text
+    })
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Issue with server response');
+      }
+      return res.json();
+    })
+    .then(data => console.log(data.message))
+    .catch(error => {
+      console.error('Error while fetching summarized text: ' + error)
+    })
   }
 
   render() {
     return (
       <div className="TextBoxContainer">
         <span className="TextBoxTitle">Summarize your text below 👇</span>
-        <form>
+        <form onSubmit={this.sendTextForSummarization}>
           <div className="FormContainer">
             <textarea
               className="TextBox"
