6f24a4c6a74a84fb78a4d22a49ea4d8662c045b4
Henry Bao <henry@bao.nyc>
2023-12-11 03:54:11 +0000

    merge conflict

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const { spawn } = require("child_process");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { spawn } = require('child_process');
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,38 +13,40 @@ app.use(cors({
 
 app.use(bodyParser.text());
 
-app.post("/summarize", (req, res) => {
+app.post('/summarize', (req, res) => {
+    console.log('request came in with:', req.body);
     // Mental note that there are different kinds of quotation marks...
     // dont waste 2 hours trying to debug this ever again
-    parsedText = req.body.replace(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, "\"");
+    parsedText = req.body.replace(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"');
 
-    const pythonProcess = spawn('python', ['./smartsummary.py', parsedText]);
+    const pythonProcess = spawn('python3', ['./smartsummary.py', parsedText]);
 
     let stdout = '';
     let stderr = '';
 
     pythonProcess.stdout.on('data', (data) => {
         stdout += data;
-    })
+    });
 
     pythonProcess.stderr.on('data', (data) => {
         stderr += data;
-    })
+    });
 
     pythonProcess.on('close', (code) => {
         if (code !== 0) {
             console.error(`Error executing Python script. Exit code: ${code}`);
-            return res.status(500).json({ error: "Server Error"});
+            console.error('Python Error:', stderr);
+            return res.status(500).json({ error: 'Server Error' });
         }
 
         console.log(stdout);
-        res.json({summary: stdout});
+        res.json({ summary: stdout });
     });
-})
+});
 
 app.get('/vercel-test', (req, res) => {
     res.send('Test for Vercel server deployment');
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
